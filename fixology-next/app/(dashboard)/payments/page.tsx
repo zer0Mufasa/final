cfe0ed7897392293f1db8f0bc75665ef773df5f2
zer0Mufasa <zeroalrashid21@gmail.com>
2025-12-30 10:07:31 -0600

    Fix payments page single export

--- fixology-next/app/(dashboard)/payments/page.tsx
+++ fixology-next/app/(dashboard)/payments/page.tsx
@@ -3,9 +3,4 @@ import { PaymentsHubPage } from './ui'
 export default function Page() {
   return <PaymentsHubPage />
 }
-import { PaymentsPage } from './ui'
-
-export default function Page() {
-  return <PaymentsPage />
-}
 
